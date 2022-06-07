f97563d7a0c168c298131044e6f9a9b2a12e7f54
fairytail <1529313064@qq.com>
2022-06-07 12:59:42 +0800

    mjj002

--- kzkt-pro/src/main.js
+++ kzkt-pro/src/main.js
@@ -3,6 +3,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//全局引入axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+axios.defaults.baseURL='http://localhost:3000'
+Vue.use(VueAxios,axios)
+
+//配置mintui
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+Vue.use(MintUI)
+
+//配置vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
+
 Vue.config.productionTip = false
 
 new Vue({
