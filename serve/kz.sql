ebaa2a3366bd566db27def73b20d9acd848868d3
fairytail <1529313064@qq.com>
2022-06-07 19:01:59 +0800

    mjj002

--- serve/kz.sql
+++ serve/kz.sql
@@ -3,8 +3,9 @@ create database kz CHARSET=UTF8;
 use kz;
 create table kz_user(
     uid INT PRIMARY KEY AUTO_INCREMENT,
+    uname VARCHAR(32),
     upwd VARCHAR(32),
     email VARCHAR(64),
     phone VARCHAR(16)
 );
-insert into kz_user values(1,'123456','126@qq.com','15878942654');
+insert into kz_user values(1,'张三','123456','126@qq.com','15878942654');
