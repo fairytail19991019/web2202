a64f20649f4008c12ca11c702ba5f3455c22d935
fairytail <1529313064@qq.com>
2022-06-08 08:46:00 +0800

    mjj003

--- serve/kz.sql
+++ serve/kz.sql
@@ -1,3 +1,4 @@
+set names utf8;
 drop database if exists kz;
 create database kz CHARSET=UTF8;
 use kz;
@@ -8,14 +9,22 @@ create table `kz_user`(
     `email` VARCHAR(64),
     `phone` VARCHAR(16)
 );
-insert into kz_user values(1,'张三','123456','126@qq.com','15878942654');
-insert into kz_user values(2,'李四','123456','1245@qq.com','15873494265');
-create table kz_category(
+insert into `kz_user` values(1,'张三','123456','126@qq.com','15878942654');
+insert into `kz_user` values(2,'李四','123456','1245@qq.com','15873494265');
+create table `kz_category`(
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `category_name` VARCHAR(30) NOT NULL
 );
-CREATE TABLE kz_class(
+insert into `kz_category` values(10,'前端开发');
+insert into `kz_category` values(20,'软件测试');
+insert into `kz_category` values(30,'英语');
+insert into `kz_category` values(40,'java');
+insert into `kz_category` values(50,'Excel');
+
+CREATE TABLE `kz_class`(
     `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `title` varchar(50) NOT NULL,
-    `image` varchar(50) DEFAULT NULL
-)
+    `category_id` INT,
+    `title` varchar(255) NOT NULL,
+    `image` varchar(255) DEFAULT NULL,
+    foreign KEY(`category_id`) references `kz_category`(`id`)
+);
