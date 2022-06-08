3beec2e0c2c51451d0892e71ad85448398e3611c
huhuhunb <2721695925@qq.com>
2022-06-08 20:07:53 +0800

    sjh6.8-2

--- kzkt-pro/src/views/MyKnowdge.vue
+++ kzkt-pro/src/views/MyKnowdge.vue
@@ -1,15 +1,31 @@
 <template>
-    <div>
-芜湖起飞
-    </div>
+  <div>
+    <van-search
+      v-model="kecheng"
+      shape="round"
+      background="#4fc08d"
+      placeholder="请搜索你想要的课程"
+    />
+    <van-grid :column-num="4">
+      <van-grid-item
+        v-for="value in 8"
+        :key="value"
+        icon="photo-o"
+        text="文字"
+      />
+    </van-grid>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+  data() {
+    return {
+      kecheng: "",
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
