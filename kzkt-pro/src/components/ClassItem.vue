1b5a63a81032ac1a3860b923fac319a48f5e31dd
zstz <1767390247@qq.com>
2022-06-09 10:46:19 +0800

    zstz001

---
+++ kzkt-pro/src/components/ClassItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="box">
+    <div>
+      <img src="/kzkt-pro/src/assets/img/dyjj01.jpg" alt="" />
+      <div class="text1">
+        <div>标题</div>
+        <div>已学1%</div>
+        <div>上次学到xxx</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+div {
+  display: flex;
+  img {
+    width: 178px;
+    height: 100px;
+  }
+  .text1 {
+    display: inline-block;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+}
+</style>
