53145db028c9637e8c1433a37bfc772494fe93ad
zstz123456 <107045016+zstz123456@users.noreply.github.com>
2022-06-11 11:31:15 +0800

    Add files via upload

--- kzkt-pro/src/components/ClassItem.vue
+++ kzkt-pro/src/components/ClassItem.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="box">
     <div>
-      <img src="/kzkt-pro/src/assets/img/dyjj01.jpg" alt="" />
+      <img src="../assets/img/01.webp" alt="" />
       <div class="text1">
-        <div>标题</div>
+        <div class="zstz-text1">
+          Python零基础新手小白入门到项目实战【六星教育】
+        </div>
         <div>已学1%</div>
         <div>上次学到xxx</div>
       </div>
@@ -19,14 +21,26 @@ export default {};
 div {
   display: flex;
   img {
-    width: 178px;
-    height: 100px;
+    width: 32vw;
+    height: 10vh;
+    margin: 10px;
+    border-radius: 5px;
   }
   .text1 {
     display: inline-block;
     display: flex;
     flex-direction: column;
     justify-content: space-around;
+    color: #b6bbbf;
+    .zstz-text1 {
+      width: 60vw;
+      display: inline-block;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+      font-weight: bold;
+      color: black;
+    }
   }
 }
 </style>
