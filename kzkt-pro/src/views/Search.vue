8752ab28d56abd61b4a1f82379503bf39c6b6487
fairytail1019 <1529313064@qq.com>
2022-06-12 18:04:21 +0800

    mjj005

---
+++ kzkt-pro/src/views/Search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <form action="/">
+      <van-search
+        v-model="value"
+        show-action
+        shape="round"
+        placeholder="请输入搜索关键词"
+        @search="onSearch"
+        @cancel="onCancel"
+      />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: "",
+    };
+  },
+  methods: {
+    onSearch(val) {
+      Toast(val);
+    },
+    onCancel() {
+      Toast("取消");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
